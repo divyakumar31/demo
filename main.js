f989a66c06ceb8268486c15e992e1f269c0d676b
divyakumar patel <d.b.patel691@gmail.com>
2023-08-05 09:56:46 +0530

    First Commit

---
+++ main.js
@@ -0,0 +1,23 @@
+let navLinks = document.querySelector('.navLinks');
+let toggleMenu = document.querySelector('.toggleNav');
+let closeMenu = document.querySelector('.closeNav');
+const submitAlert = document.querySelector('.submit');
+const darkMode = document.querySelector('.darkMode');
+
+toggleMenu.addEventListener('click', ()=> {
+   navLinks.classList.add('active');
+})
+
+closeMenu.addEventListener('click', ()=> {
+    navLinks.classList.remove('active');
+})
+
+submitAlert.addEventListener('click', ()=> {
+    alert("Sorry, Can't submit your message.")
+})
+
+darkMode.addEventListener('click', ()=> {
+    document.body.classList.toggle('dark-theme-colors');
+    darkMode.querySelector('i:nth-child(1)').classList.toggle('active');
+    darkMode.querySelector('i:nth-child(2)').classList.toggle('active');
+});
