a486591522a00c4da5f85a177a9a2fb1c787188a
Divya Patel <121997327+divyakumar31@users.noreply.github.com>
2023-09-01 05:31:46 +0000

    Skills logo set and form response enabled

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 let navLinks = document.querySelector('.navLinks');
 let toggleMenu = document.querySelector('.toggleNav');
 let closeMenu = document.querySelector('.closeNav');
-const submitAlert = document.querySelector('.submit');
+// const submitAlert = document.querySelector('.submit');
 const darkMode = document.querySelector('.darkMode');
 
 toggleMenu.addEventListener('click', ()=> {
@@ -12,9 +12,9 @@ closeMenu.addEventListener('click', ()=> {
     navLinks.classList.remove('active');
 })
 
-submitAlert.addEventListener('click', ()=> {
-    alert("Sorry, Can't send your message. 🙂")
-})
+// submitAlert.addEventListener('click', ()=> {
+//     alert("Sorry, Can't send your message. 🙂")
+// })
 
 darkMode.addEventListener('click', ()=> {
     document.body.classList.toggle('dark-theme-colors');
@@ -28,4 +28,27 @@ if (prefersColorScheme.matches) {
     document.body.classList.toggle('dark-theme-colors');
     darkMode.querySelector('i:nth-child(1)').classList.toggle('active');
     darkMode.querySelector('i:nth-child(2)').classList.toggle('active');
-} 
+}
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbz3L6ciV-NyRKihtai1ILPoHmhoYmRf0YsFoZOmeIfSFKF1os5WSbie0Acz0eZnRF_ZMg/exec'
+const form = document.forms['submit-to-google-sheet']
+const sendername = document.getElementById("name")
+const mail = document.getElementById("mail")
+const message = document.getElementById("message")
+
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    alert("Do you want to submit your message?")
+    fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+        .then(response => {
+            alert("Message sent sucessfully")
+            // message.innerHTML = "Message sent sucessfully"
+            setTimeout(function () {
+                sendername.innerHTML = ""
+                mail.innerHTML = ""
+                message.innerHTML = ""
+            }, 5000)
+            form.reset()
+        })
+        .catch(error => console.error('Error!', error.message))
+})
