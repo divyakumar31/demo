c11a8d5f66afdae1d5b2f6f82d3ebac87415d484
Divya Patel <121997327+divyakumar31@users.noreply.github.com>
2023-09-04 18:12:35 +0000

    New UI

--- main.js
+++ main.js
@@ -1,25 +1,25 @@
 let navLinks = document.querySelector('.navLinks');
 let toggleMenu = document.querySelector('.toggleNav');
 let closeMenu = document.querySelector('.closeNav');
-// const submitAlert = document.querySelector('.submit');
 const darkMode = document.querySelector('.darkMode');
+const projectImages = document.querySelectorAll('.project-img-link');
 
-toggleMenu.addEventListener('click', ()=> {
-   navLinks.classList.add('active');
+toggleMenu.addEventListener('click', () => {
+    navLinks.classList.add('active');
 })
 
-closeMenu.addEventListener('click', ()=> {
+closeMenu.addEventListener('click', () => {
     navLinks.classList.remove('active');
 })
 
-// submitAlert.addEventListener('click', ()=> {
-//     alert("Sorry, Can't send your message. 🙂")
-// })
-
-darkMode.addEventListener('click', ()=> {
+darkMode.addEventListener('click', () => {
     document.body.classList.toggle('dark-theme-colors');
     darkMode.querySelector('i:nth-child(1)').classList.toggle('active');
     darkMode.querySelector('i:nth-child(2)').classList.toggle('active');
+    for (let projectImage of projectImages){
+        projectImage.querySelector('img:nth-child(1)').classList.toggle('active');
+        projectImage.querySelector('img:nth-child(2)').classList.toggle('active');
+    }
 });
 
 const prefersColorScheme = window.matchMedia('(prefers-color-scheme: dark)');
@@ -28,6 +28,10 @@ if (prefersColorScheme.matches) {
     document.body.classList.toggle('dark-theme-colors');
     darkMode.querySelector('i:nth-child(1)').classList.toggle('active');
     darkMode.querySelector('i:nth-child(2)').classList.toggle('active');
+    for (let projectImage of projectImages){
+        projectImage.querySelector('img:nth-child(1)').classList.toggle('active');
+        projectImage.querySelector('img:nth-child(2)').classList.toggle('active');
+    }
 }
 
 const scriptURL = 'https://script.google.com/macros/s/AKfycbz3L6ciV-NyRKihtai1ILPoHmhoYmRf0YsFoZOmeIfSFKF1os5WSbie0Acz0eZnRF_ZMg/exec'
@@ -42,12 +46,11 @@ form.addEventListener('submit', e => {
     fetch(scriptURL, { method: 'POST', body: new FormData(form) })
         .then(response => {
             alert("Message sent sucessfully")
-            // message.innerHTML = "Message sent sucessfully"
             setTimeout(function () {
                 sendername.innerHTML = ""
                 mail.innerHTML = ""
                 message.innerHTML = ""
-            }, 5000)
+            }, 1000)
             form.reset()
         })
         .catch(error => console.error('Error!', error.message))
